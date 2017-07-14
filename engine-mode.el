08d1a52ce65ef4b2c40f378843a393032166518d
Basil L. Contovounesios <contovob@tcd.ie>
2017-07-14 10:49:17 +0200

    Autoload engine-mode

--- engine-mode.el
+++ engine-mode.el
@@ -55,6 +55,7 @@
 (define-prefix-command 'engine-mode-prefixed-map)
 (defvar engine-mode-prefixed-map)
 
+;;;###autoload
 (define-minor-mode engine-mode
   "Minor mode for defining and querying search engines through Emacs.
 
