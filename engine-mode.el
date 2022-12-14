a5f1ae2b087109f37a704c496942cec9d9f48698
Stefan Kangas <stefankangas@gmail.com>
2022-12-14 08:47:55 +0100

    Bump version to 2.2.1

--- engine-mode.el
+++ engine-mode.el
@@ -1,7 +1,7 @@
 ;;; engine-mode.el --- Define and query search engines from within Emacs
 
 ;; Author: Harry R. Schwartz <hello@harryrschwartz.com>
-;; Version: 2.2.0
+;; Version: 2.2.1
 ;; URL: https://github.com/hrs/engine-mode
 ;; Package-Requires: ((cl-lib "0.5"))
 
