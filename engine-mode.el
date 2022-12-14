48f32a7478cbf0e88494fc5c18389dddbbec3c61
Stefan Kangas <stefankangas@gmail.com>
2022-12-14 08:47:46 +0100

    Prefer HTTPS to HTTP

--- engine-mode.el
+++ engine-mode.el
@@ -159,7 +159,7 @@ Keybindings are in the `engine-mode-map', so they're prefixed.
 For example, to search Wikipedia, use:
 
   (defengine wikipedia
-    \"http://www.wikipedia.org/search-redirect.php?language=en&go=Go&search=%s\"
+    \"https://www.wikipedia.org/search-redirect.php?language=en&go=Go&search=%s\"
     :keybinding \"w\"
     :docstring \"Search Wikipedia!\")
 
