f955a5b889aa54ed5cd89bb31b1003f1977b3482
Stefan Kangas <stefankangas@gmail.com>
2022-12-14 08:43:48 +0100

    Fix issues found by package-lint

--- engine-mode.el
+++ engine-mode.el
@@ -1,4 +1,4 @@
-;;; engine-mode.el --- Define and query search engines from within Emacs.
+;;; engine-mode.el --- Define and query search engines from within Emacs
 
 ;; Author: Harry R. Schwartz <hello@harryrschwartz.com>
 ;; Version: 2.2.0
@@ -6,6 +6,19 @@
 ;; Package-Requires: ((cl-lib "0.5"))
 
 ;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -33,24 +46,13 @@
 ;; `C-x / d' is now bound to the new function
 ;; engine/search-duckduckgo'! Nifty.
 
-;;; License:
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 ;;; Code:
-(eval-when-compile (require 'cl-macs))
+(eval-when-compile (require 'cl-lib))
 (require 'format-spec)
+
+(defgroup engine-mode nil
+  "Define search engines, bind them to keybindings, and query them."
+  :group 'external)
 
 (defcustom engine/keybinding-prefix "C-x /"
   "The default engine-mode keybindings prefix."
